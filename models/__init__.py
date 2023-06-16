905e01b9010b5cecfb7463abf8275cda5c47417d
worawut aunkham <jameskater350@gmail.com>
2023-06-16 14:15:33 +0700

    Create environment for auth feature

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+import bcrypt
+
+from models.user import User
+from sqlalchemy import event
+from .database import db
+
+
+@event.listens_for(User.__table__, 'after_create')
+def create_user(*args, **kwargs):
+    db.session.add(User(username='user', password=bcrypt.hashpw('password'.encode('utf-8'),bcrypt.gensalt(10)),anime_id=None))
+    db.session.commit()
---
+++ models/user.py
@@ -0,0 +1,23 @@
+from .database import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    anime_id = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, username, password, anime_id):
+        self.username = username
+        self.password = password
+        self.anime_id = anime_id
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'anime_id': self.anime_id
+        }

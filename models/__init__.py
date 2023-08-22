527a0eac6752efd69cc0dcb046da99c502968ba9
worawut aunkham <jameskater350@gmail.com>
2023-08-22 17:06:06 +0700

    Add new user detail for example

--- models/__init__.py
+++ models/__init__.py
@@ -8,4 +8,5 @@ from .database import db
 @event.listens_for(User.__table__, 'after_create')
 def create_user(*args, **kwargs):
     db.session.add(User(username='user', password=bcrypt.hashpw('password'.encode('utf-8'),bcrypt.gensalt(10)),distance='440'))
+    db.session.add(User(username='user01', password=bcrypt.hashpw('user01'.encode('utf-8'), bcrypt.gensalt(10)), distance='380'))
     db.session.commit()
